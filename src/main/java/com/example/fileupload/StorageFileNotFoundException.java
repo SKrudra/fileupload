18ab0a4f5f85cb7bdf9b958bd4858ae9bdc52942
Sandeep Kumar <skrudra12@gmail.com>
2019-12-14 23:52:14 +0530

    Initial commit

---
+++ src/main/java/com/example/fileupload/StorageFileNotFoundException.java
@@ -0,0 +1,16 @@
+package com.example.fileupload;
+
+public class StorageFileNotFoundException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public StorageFileNotFoundException(String message) {
+		super(message);
+	}
+
+	public StorageFileNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
